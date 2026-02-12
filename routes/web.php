cf5c4385a5600906105b93db28a8c6915aa48997
JaimeMrF <jaime.ja203@gmail.com>
2026-02-12 11:58:33 -0500

    php basico

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,52 @@ Route::get('/mi-nombre', function () {
     return 'JAIME ALEJANDRO VEGA BARBOSA';
 });
 
-Route::get('/php-basico...', function () {
+Route::get('/php-basico', function () {
 
+
+    echo "<h1 style='color:red';>Aprendiendo PHP</h1>";
+    $name = "Jaime Vega";
+    $height = 1.74;
+    $isLogin = true;
+    $age = 26;
+
+    echo "<h4> " . $name . " </h4>";
+    echo "<p> " . $name . " </p>";
     
-    return 'JAIME ALEJANDRO VEGA BARBOSA';
+    echo "<br><br><br> ****************** ESTRUCTURAS DE CONTROL ******************<br><br><br>";
+
+    $message = "Soy $name, tengo $age años. ". validateAge($age);
+
+
+    $message .= $isLogin ? " Estoy logeado" : " No estoy logeado";
+
+    echo $message;
+
+    echo "<br><br><br> ****************** ESTRUCTURAS DE DATOS ******************<br><br><br>";
+
+    $pc = [
+        "name" => "pc gamer core9",
+        "price" => 6000,
+        "marca" => "Lenovo"
+    ];
+
+    $teclado = [
+        "name" => "Teclado",
+        "price" => 200,
+        "marca" => "Asus"
+    ];
+
+    $lista_productos = [$pc, $teclado];
+
+    foreach ($lista_productos as $item) {
+        echo $item['name'] ."<br>";
+    };
 });
 
+function validateAge($age) {
+    if ($age >= 18) {
+        return " Soy mayor de edad";
+    } else {
+        return " Soy menor de edad";
+    };
+}
